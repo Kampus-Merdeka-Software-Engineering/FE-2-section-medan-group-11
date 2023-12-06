d0f988d9e95522c15ca8ba11af204d0de63361d3
Aulia Rahman Maulana <gtrrahman235@gmail.com>
2023-12-06 19:44:30 +0700

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,147 @@
+function goToKota() {
+    window.location.href = "./FE/page/kota.html";
+    return false;
+}
+
+function goToKos() {
+    window.location.href = "jakarta.html";
+    return false;
+}
+
+function goToHome() {
+    window.location.href = "index.html";
+    return false;
+}
+
+function goToKosPage() {
+    window.location.href = "kos.html";
+    return false;
+}
+
+function goToKosInfo() {
+    window.location.href = "information.html";
+    return false;
+}
+
+document.getElementById('pesanButton').addEventListener('click', function () {
+    window.location.href = 'booked.html'; // Ganti 'menu_pemesanan.html' dengan halaman yang sesuai
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                const targetOffset = targetSection.offsetTop - document.querySelector('header').offsetHeight;
+                const initialPosition = window.scrollY;
+                const duration = 800; // Waktu animasi dalam milidetik
+                let startTime;
+
+                function scrollAnimation(currentTime) {
+                    if (!startTime) startTime = currentTime;
+                    const progress = currentTime - startTime;
+
+                    window.scrollTo(0, easeInOutCubic(progress, initialPosition, targetOffset, duration));
+
+                    if (progress < duration) {
+                        requestAnimationFrame(scrollAnimation);
+                    }
+                }
+
+                // Fungsi bantu untuk interpolasi linier
+                function easeInOutCubic(t, b, c, d) {
+                    t /= d / 2;
+                    if (t < 1) return c / 2 * t * t * t + b;
+                    t -= 2;
+                    return c / 2 * (t * t * t + 2) + b;
+                }
+
+                requestAnimationFrame(scrollAnimation);
+            }
+        });
+    });
+
+    // Your custom carousel logic
+    const teamCarousel = document.querySelector(".team-carousel");
+    const arrowLeft = document.querySelector(".fa-arrow-left");
+    const arrowRight = document.querySelector(".fa-arrow-right");
+
+    let translateValue = 0;
+    const itemWidth = document.querySelector(".team-item").offsetWidth + 15; // Adjust the value based on your design
+
+    arrowLeft.addEventListener("click", function () {
+        translateValue += itemWidth;
+        updateCarousel();
+    });
+
+    arrowRight.addEventListener("click", function () {
+        translateValue -= itemWidth;
+        updateCarousel();
+    });
+
+    function updateCarousel() {
+        const maxTranslate = (teamCarousel.children.length - 5) * itemWidth; // Adjust the value based on the number of visible items
+        if (translateValue > 0) {
+            translateValue = -maxTranslate;
+        } else if (translateValue < -maxTranslate) {
+            translateValue = 0;
+        }
+        teamCarousel.style.transform = `translateX(${translateValue}px)`;
+    }
+
+    // Owl Carousel initialization for ".team-carousel"
+    $(".team-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText: [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+
+    // Owl Carousel initialization for ".testimonial-carousel"
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: true,
+        loop: true,
+        center: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+});
